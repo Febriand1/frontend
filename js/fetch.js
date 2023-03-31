1badc46a4b69730a1163cb1612e9c3b7c1e9a5a4
Febriand1 <fdirga63@gmail.com>
2023-03-31 15:24:40 +0700

    budiiiiiii

--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,4 @@
+// data array
 // import { get } from "https://bukulapak.github.io/api/process.js";
 // let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
 // get(urlAPI, isiTablePresensi);
@@ -5,14 +6,37 @@
 //   console.log(results);
 // }
 
+// data row
+// import { get } from "https://bukulapak.github.io/api/process.js";
+// import { setInner } from "https://bukulapak.github.io/element/process.js";
+// let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
+// get(urlAPI, isiTablePresensi);
+// function isiTablePresensi(results) {
+//   console.log(results);
+//   results.forEach(isiRow);
+// }
+// function isiRow(value) {
+//   console.log(value);
+// }
+
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
-let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./temp/table.js";
+let urlAPI = "https://ws-ulbi.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
-  console.log(results);
   results.forEach(isiRow);
 }
 function isiRow(value) {
-  console.log(value);
+  let content = isiTabel
+    .replace("#NAMA#", value.biodata.nama)
+    .replace("#NOHP#", value.biodata.phone_number)
+    .replace("#JABATAN#", value.biodata.jabatan)
+    .replace("#LOKASI#", value.location)
+    .replace("#STATUS#", value.checkin)
+    .replace("#HARIKERJA#", value.biodata.hari_kerja)
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
 }
