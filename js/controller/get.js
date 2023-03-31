dc434f784c8f04e1d3a28b4d7f60dd9cfa56f0ab
Febriand1 <fdirga63@gmail.com>
2023-03-31 16:48:05 +0700

    budiiiiii

--- js/controller/get.js
+++ js/controller/get.js
@@ -6,15 +6,15 @@ export function isiTablePresensi(results) {
 }
 function isiRow(value) {
   let content = isiTabel
-    .replace("NAMA", value.biodata.nama)
-    .replace("NOHP", value.biodata.phone_number)
-    .replace("JABATAN", value.biodata.jabatan)
-    .replace("LOKASI", value.location)
-    .replace("STATUS", value.checkin)
-    .replace("HARIKERJA", value.biodata.hari_kerja)
-    .replace("JAMKERJA", value.biodata.jam_kerja.durasi)
-    .replace("JAMMASUK", value.biodata.jam_kerja.jam_masuk)
-    .replace("JAMKELUAR", value.biodata.jam_kerja.jam_keluar)
+    .replace("#NAMA#", value.biodata.nama)
+    .replace("#NOHP#", value.biodata.phone_number)
+    .replace("#JABATAN#", value.biodata.jabatan)
+    .replace("#LOKASI#", value.location)
+    .replace("#STATUS#", value.checkin)
+    .replace("#HARIKERJA#", value.biodata.hari_kerja)
+    .replace("#JAMKERJA#", value.biodata.jam_kerja.durasi)
+    .replace("#JAMMASUK#", value.biodata.jam_kerja.jam_masuk)
+    .replace("#JAMKELUAR#", value.biodata.jam_kerja.jam_keluar)
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
