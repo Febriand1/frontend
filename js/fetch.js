2f4e34f86b24a477df28220e0b0c715d13117b35
Febriand1 <fdirga63@gmail.com>
2023-03-31 16:44:22 +0700

    budi

--- js/fetch.js
+++ js/fetch.js
@@ -43,6 +43,6 @@
 // }
 
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiTablePresensi } from "./controller/table.js";
+import { isiTablePresensi } from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
 get(urlAPI, isiTablePresensi);
