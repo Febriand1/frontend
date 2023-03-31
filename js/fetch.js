9bbcc61d4101c9b5c08283afed77994536af7c19
Febriand1 <fdirga63@gmail.com>
2023-03-31 15:27:29 +0700

    budiiii

--- js/fetch.js
+++ js/fetch.js
@@ -22,7 +22,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "./temp/table.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
