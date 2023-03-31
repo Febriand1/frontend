582d5ef54942bba5e498821adaa07b9c9d14e3ed
Febriand1 <fdirga63@gmail.com>
2023-03-31 15:17:40 +0700

    budiiii

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,18 @@
+// import { get } from "https://bukulapak.github.io/api/process.js";
+// let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
+// get(urlAPI, isiTablePresensi);
+// function isiTablePresensi(results) {
+//   console.log(results);
+// }
+
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ws-nilai.herokuapp.com/presensinew";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
   console.log(results);
+  results.forEach(isiRow);
+}
+function isiRow(value) {
+  console.log(value);
 }
