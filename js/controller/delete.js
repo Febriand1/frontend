b761a9b956c064a5adf4cfe23452fa3d083480b9
Febriand1 <fdirga63@gmail.com>
2023-05-19 16:00:19 +0700

    wah

--- js/controller/delete.js
+++ js/controller/delete.js
@@ -1,18 +1,20 @@
-function deleteData() {
-  var presensiId = document.querySelector("a[data-presensi-id]").getAttribute("data-presensi-id");
-  console.log(presensiId);
-  //   var target_url = "https://ws-nilai.herokuapp.com/delete/" + presensiId;
+function deleteData(IDHAPUS) {
+  //   var presensiId = document.querySelector("a[data-presensi-id]").getAttribute("data-presensi-id");
+  //   console.log(presensiId);
+  var presensiId = IDHAPUS;
+  console.log(IDHAPUS);
+  var target_url = "https://ws-nilai.herokuapp.com/delete/" + presensiId;
 
-  //   var requestOptions = {
-  //     method: "DELETE",
-  //     redirect: "follow",
-  //   };
+  var requestOptions = {
+    method: "DELETE",
+    redirect: "follow",
+  };
 
-  //   fetch(target_url, requestOptions)
-  //     .then((response) => response.json())
-  //     .then((result) => {
-  //       alert(result.message);
-  //       location.reload();
-  //     })
-  //     .catch((error) => console.log("Error:", error));
+  fetch(target_url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      alert(result.message);
+      location.reload();
+    })
+    .catch((error) => console.log("Error:", error));
 }
