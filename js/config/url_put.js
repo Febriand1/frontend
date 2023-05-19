0d0ff583fa4059851234a98d270a13dcbc20c29e
Febriand1 <fdirga63@gmail.com>
2023-05-19 15:13:01 +0700

    wah

---
+++ js/config/url_put.js
@@ -0,0 +1,11 @@
+//mendapatkan paramerter dari url
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get("presensiId");
+
+export let urlFetch = "https://ws-nilai.herokuapp.com/upd/" + presensiId;
+
+export function AmbilResponse(result) {
+  console.log(result); //menampilkan response API pada console
+  alert(result.message); //menampilkan response API pada alert
+  window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
+}
